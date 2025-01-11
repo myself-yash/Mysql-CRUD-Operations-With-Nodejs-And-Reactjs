23f6da4a7d07bbeaf5c4134af26909ad89f8f98c
Yash Mittal <yashmittal258@gmail.com>
2025-01-11 16:28:39 +0530

    update table in frontend

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -16,7 +16,7 @@ function Table() {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get("http://localhost:3000");
+        const response = await axios.get("http://backend:3000");
         //setting the empty array as a json object of users got from the server
         setUsers(response.data);
 
